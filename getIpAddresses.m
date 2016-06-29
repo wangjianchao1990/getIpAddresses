8ba31269b7959c9304b4181a1269149fdb61d244
wangjianchao1990 <347458101@qq.com>
2016-06-29 17:08:37 +0800

    getIpAddresses
    getIpAddresses获取设备IP地址

---
+++ getIpAddresses.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  timeStamp
+//
+//  Created by MAC on 16/6/29.
+//  Copyright © 2016年 银资. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <ifaddrs.h>
+#import <arpa/inet.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+//获取ip地址
+- (NSString *)getIpAddresses{
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0)
+    {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL)
+        {
+            if(temp_addr->ifa_addr->sa_family == AF_INET)
+            {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
+                {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    return address;
+}
+
+@end
